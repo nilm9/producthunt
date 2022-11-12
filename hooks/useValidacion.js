1eae8fcdf7ce7e39613ae42f083d7cef3950f0a0
Nil Monfort <nil.monfort@estudiants.urv.cat>
2022-11-12 17:36:51 +0100

    new pages, styles, starting hook

---
+++ hooks/useValidacion.js
@@ -0,0 +1,31 @@
+import { calculateSizeAdjustValues } from "next/dist/server/font-utils";
+import { useState, useEffect } from "react";
+
+const useValidacion = (stateInicial, validar, fn) => {
+    const [valores, setValores] = useState([]) = useState(stateInicial);
+    const [errores, setErrores] = useState([]) = useState({});
+    const [submitForm, setSubmitForm] = useState(false)
+
+    useEffect(()=>{
+        if(submitForm){
+            const noErrores = Object.keys(errores).length==0;
+            if(noErrores){
+                fn(); 
+            }
+            setSubmitForm(false);
+        }
+    }, []);
+
+
+    //Funcion que se ejecuta conforme el usuario escribe algo
+    const handleCahnge = e => {
+        setValores({
+            ...valores,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    return();
+}
+
+export default useValidacion;
